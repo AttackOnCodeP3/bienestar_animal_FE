f6d3e7fbdf5b3c8dd754b53bf05dab905a99b815
David Gutiérrez <148672101+ddgutierrezc@users.noreply.github.com>
2025-06-23 13:59:24 -0600

    Style settings are adjusted and themes are added for the mat toolbar.

--- src/app/pages/gamification/rewards-system/rewards-system.ts
+++ src/app/pages/gamification/rewards-system/rewards-system.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-rewards-system',
   imports: [
-    MatFormFieldModule, MatInputModule, MatIconModule
+    MatFormFieldModule, MatInputModule, MatIconModule, MatButton
   ],
   templateUrl: './rewards-system.html',
   styleUrl: './rewards-system.scss'
