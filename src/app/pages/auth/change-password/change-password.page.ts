13af1687dd8c7211b4f1ccf582f99235f7c85da4
Adrian Blanco Cordero <63083462+Ablanco1547@users.noreply.github.com>
2025-07-13 18:27:13 -0400

    Remove unused TranslatePipe import
    The TranslatePipe import was removed from the change-password page as it is no longer used. This helps clean up unnecessary dependencies.

--- src/app/pages/auth/change-password/change-password.page.ts
+++ src/app/pages/auth/change-password/change-password.page.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { PasswordResetComponent } from '@components/forms/user/password-reset/password-reset.component';
-import { TranslatePipe } from '@ngx-translate/core';
+
 import { AlertService, I18nService } from '@services/general';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.page.html',
   styleUrls: ['./change-password.page.scss'],
-  imports: [TranslatePipe, PasswordResetComponent]
+  imports: [ PasswordResetComponent]
 })
 export class ChangePassword {
  readonly i18nService = inject(I18nService);
